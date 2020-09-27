55cac6bb1337d82c94915f8e2f8a77fd253f449b
John Wolfe <jwolfe890@gmail.com>
2020-09-26 22:36:35 -0500

    update

--- test/controllers/CarsController.test.js
+++ test/controllers/CarsController.test.js
@@ -1,1 +1,24 @@
+/* eslint-disable */
 
+var supertest = require("supertest");
+var assert = require("assert");
+var createdSegment;
+require("../bootstrap.test");
+
+describe("Car Controller", function () {
+  it("get /car", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .get("/car")
+      .send()
+      .expect(200)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          result.body.length.should.be.aboveOrEqual(0);
+          done();
+        }
+      });
+  });
+});
